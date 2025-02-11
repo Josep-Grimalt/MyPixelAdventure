3b81f97475b716b8e425660b2a7babd4c443a643
Josep-Grimalt <josepmasg@gmail.com>
2025-02-11 19:26:28 +0100

    Update PlayerMngr.cs

--- Assets/Scripts/DinoAdventure/PlayerMngr.cs
+++ Assets/Scripts/DinoAdventure/PlayerMngr.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class PlayerMngr : MonoBehaviour
 {
@@ -19,7 +16,6 @@ public class PlayerMngr : MonoBehaviour
     private bool canDash;
 
     private int lifes;
-    private bool isDead;
     private bool isPlayerReady;
 
     void Start()
